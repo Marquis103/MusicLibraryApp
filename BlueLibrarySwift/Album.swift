e30d48afa8bc53930d236e59b3a4a3b8099d9c63
Marquis Dennis <marquis103@batcave.fios-router.home>
2015-12-14 23:57:47 -0500

    Added gitignore

--- BlueLibrarySwift/Album.swift
+++ BlueLibrarySwift/Album.swift
@@ -9,5 +9,36 @@
 import UIKit
 
 class Album: NSObject {
+    var title: String!
+    var artist: String!
+    var genre: String!
+    var coverUrl: String!
+    var year: String!
+    
+    init(title: String, artist: String, genre: String, coverUrl: String, year: String) {
+        super.init()
+        
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.coverUrl = coverUrl
+        self.year = year
+    }
+    
+    //string representation of the Album's attributes
+    override var description: String {
+        return "title: \(title)" +
+        "artist: \(artist)" +
+        "genre: \(genre)" +
+        "coverUrl: \(coverUrl)" +
+        "year: \(year)"
+    }
 
+}
+
+//a way to implement decorator pattern in ios (extensions)
+extension Album {
+    func ae_tableRepresentation() -> (titles:[String], values:[String]) {
+        return(["Artist", "Album", "Genre", "Year"], [artist, title, genre, year])
+    }
 }
--- BlueLibrarySwift/LibraryAPI.swift
+++ BlueLibrarySwift/LibraryAPI.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class LibraryAPI: NSObject {
-
+    
+    //implement singleton design pattern
+    static let sharedInstance: LibraryAPI = LibraryAPI()
+    
+    private let persistencyManager: PersistencyManager
+    private let httpClient: HTTPClient
+    private let isOnline:Bool
+    
+    override init() {
+        persistencyManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
+    
+    //:MARK - LibraryAPI Facade
+    func getAlbums() -> [Album] {
+        return persistencyManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int) {
+        persistencyManager.addAlbum(album, index: index)
+        
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description)
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistencyManager.deleteAlbumAtIndex(index)
+        
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: ("\(index)"))
+        }
+    }
 }
